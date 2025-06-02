3bfbb7807e2e7c2511e412147f7961f4cb3576c0
Djennkins <djennkins228@gmail.com>
2025-06-02 15:31:25 +0300

    added page header component

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,10 @@
 import Dashboard from '@/components/Dashboard';
+import PageHeader from '@/components/PageHeader';
 
 export default function DashboardPage() {
     return (
-        <div>
-            DashBoardPage
-            {/*PageHeader*/}
+        <div className="py-10 px-8 bg-[#F7F9FD] flex-1">
+            <PageHeader title="My Tasks" />
             <Dashboard />
         </div>
     );
--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,9 +1,9 @@
+import PageHeader from '@/components/PageHeader';
+
 export default function SettingsPage() {
     return (
-        <div>
-            SettingsPage
-            {/*PageHeader*/}
-            {/*  User Settings  */}
+        <div className="py-10 px-8 bg-[#F7F9FD] flex-1">
+            <PageHeader title="Settings" />
         </div>
     );
 }
