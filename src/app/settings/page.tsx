0429096ae6341e657a15fa127afe861f8bcd3d1e
Djennkins <djennkins228@gmail.com>
2025-06-02 17:09:17 +0300

    created form for saving password

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,9 +1,11 @@
 import PageHeader from '@/components/PageHeader';
+import SavePasswordForm from '@/components/SavePasswordForm';
 
 export default function SettingsPage() {
     return (
         <div className="py-10 px-8 bg-[#F7F9FD] flex-1">
             <PageHeader title="Settings" />
+            <SavePasswordForm />
         </div>
     );
 }
