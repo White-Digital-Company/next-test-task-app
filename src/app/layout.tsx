f6f82b6fecfb2975ec031d2c175cca0e7c201b77
Oleg Razor <Oleg>
2025-06-06 13:12:24 +0300

    refactor: moved QueryClientProvider to separate client component to enable server RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
-'use client';
 import './globals.css';
+import { QueryProvider } from '@/components/QueryProvider';
 import Sidebar from '@/components/SideBar';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { useState } from 'react';
 import { Poppins } from 'next/font/google';
 import { Noto_Sans } from 'next/font/google';
 
@@ -25,18 +23,16 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const [queryClient] = useState(() => new QueryClient());
-
   return (
     <html
       lang="en"
       className={`${poppins.variable} ${notoSans.variable}  h-full`}
     >
       <body className="flex min-h-screen h-full">
-        <QueryClientProvider client={queryClient}>
+        <QueryProvider>
           <Sidebar />
           <main className="flex-1 overflow-auto">{children}</main>
-        </QueryClientProvider>
+        </QueryProvider>
       </body>
     </html>
   );
